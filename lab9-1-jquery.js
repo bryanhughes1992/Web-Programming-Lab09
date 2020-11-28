302b598b1f74a2491a93596c569db9166b7cb21b
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-27 21:54:33 -0500

    Part 1 - Finished

--- lab9-1-jquery.js
+++ lab9-1-jquery.js
@@ -5,15 +5,22 @@
     PART ONE - CREATE SLIDING PANELS FOR THE CONTENT BOXES
 */
 //When DOM is loaded, fire this anonymous function
-jQuery(document).ready(function() {
+$(document).ready(function() {
     //Automatically hide all the paragraph content
-    $("p").hide();
-
+    $('p').hide();
 
     //When heading is clicked, hide paragraph content
     $('h2').click(function(){
-        $(this).next('p').slideToggle(1500);
         $('p').hide();
+        $(this).next('p').toggle(3000);
     });
 
+    $('p').hover(
+        function(){
+            $(this).css({'background': '#FFE5C3', 'color': '#524737'});
+        },
+        function(){
+            $(this).css({'background': '#524737', 'color': '#FFE5C3'});
+        }
+    );
 });
