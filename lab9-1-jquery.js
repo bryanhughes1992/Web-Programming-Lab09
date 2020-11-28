ee1a918bf92ae8929f26502c4626d3eef2089151
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-28 17:02:11 -0500

    Final Changes - Rough Copy

--- lab9-1-jquery.js
+++ lab9-1-jquery.js
@@ -1,24 +1,30 @@
 //LAB 9 - 1 FAQ PAGE
 // BRYAN N. HUGHES
 
-/*
-    PART ONE - CREATE SLIDING PANELS FOR THE CONTENT BOXES
-*/
+/**************************************************
+    CREATE SLIDING PANELS FOR THE CONTENT BOXES
+***************************************************/
+
 //When DOM is loaded, fire this anonymous function
 $(document).ready(function() {
     //Automatically hide all the paragraph content
     $('p').hide();
-
     //When heading is clicked, hide paragraph content
     $('h2').click(function(){
         $('p').hide();
         $(this).next('p').toggle(3000);
     });
 
+    /***************************
+        CHANGE COLOR ON HOVER
+    ****************************/
+   //Hover in
     $('p').hover(
         function(){
             $(this).css({'background': '#FFE5C3', 'color': '#524737'});
         },
+
+    //Hover out
         function(){
             $(this).css({'background': '#524737', 'color': '#FFE5C3'});
         }
--- lab9-2-jquery.js
+++ lab9-2-jquery.js
@@ -1,2 +1,52 @@
-//LAB 9 - 2 INVENTORY PAGE
-alert("2 - connected");
+/*
+    LAB 9 - 2 INVENTORY PAGE
+    STUDENT: BRYAN HUGHES
+*/
+
+$(document).ready(function () {
+    /*********************************
+        ONLOAD, HIDE THE DESCRIPTION
+    **********************************/
+    $('.desc').hide();
+
+    /*
+        ***FAILED ATTEMPTS***
+        $('td').hide(); ?? why this
+        $('#desc').hide();
+        $('td').next(function () {
+            $('#desc').hide();
+        });
+        SPENT A LOT OF TIME ON MESSING AROUND
+        WITH THIS, IT ENDED UP BEING THE '.' BEFORE
+        'desc'
+    */
+
+    /*****************
+        HOVER EVENTS
+    ******************/
+   $('tr').hover(
+        //Change color to red on hover
+        function(){
+            $(this).css({"background": "red", "color": "white"});
+        },
+        //Change the color back off hover
+        function(){
+            $(this).css({"background": "white", "color": "black"});
+        });
+
+    /*****************
+        CLICK EVENTS
+    ******************/
+   //Display single row of data onclick
+    $('tr').click(function() {
+        //find the '.desc' of 'this' row and show it
+        $(this).find('.desc').show();
+        //secondary click function nested within
+        $('tr').click(function () {
+            //when another 'tr' is clicked, hide the '.desc'
+            $('.desc').hide();
+            //find the '.desc' of the row clicked and show it
+            $(this).find('.desc').show();
+        });
+    });
+});
